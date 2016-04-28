40c7de9a515f7baecda2ea130632ccb3705f0066
migue.masx@gmail.com <migue.masx@gmail.com>
2016-04-28 12:09:29 +0200

    ..

--- app/assets/javascripts/components/search_input.jsx
+++ app/assets/javascripts/components/search_input.jsx
@@ -2,7 +2,7 @@ class SearchInput extends React.Component{
   render() {
     return (
       <div className="input-group col-md-8 col-md-offset-2">
-        <input type="text" className="search-query form-control" name="q" autoComplete="off" onKeyUp={this.keyUpHandler} placeholder="Search" />
+        <input type="text" className="search-query form-control" name="q" autoComplete="off" onKeyUp={this.props.keyUpHandler} placeholder="Search" />
         <span className="input-group-btn">
           <button className="btn btn-info" type="submit">
             <span className="glyphicon glyphicon-search" />
