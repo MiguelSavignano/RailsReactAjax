f84ee22c6cc1879a197eb71e978dfa8971157ad7
migue.masx@gmail.com <migue.masx@gmail.com>
2016-04-29 01:26:23 +0200

    heart example

--- app/assets/javascripts/components/favorite_btn.jsx
+++ app/assets/javascripts/components/favorite_btn.jsx
@@ -3,13 +3,13 @@ class FavoriteBtn extends React.Component {
     super(props)
     this.state = {active: this.props.active}
   }
-  _onClickHandler = (event)=> {
+  _onClickHandler = (event) => {
     this.setState({active: !this.state.active})
   }
   render () {
     return (
       <a className="icon-lg" onClick={this._onClickHandler}>
-        <i className={"blue fa "+(this.state.active ? "fa-heart " : "fa-heart-o")}></i>
+        <i className={`blue fa ${this.state.active ? 'fa-heart' : 'fa-heart-o'}`}></i>
       </a>
     );
   }
